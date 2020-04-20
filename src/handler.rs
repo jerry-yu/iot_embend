a910f9e819a6fe50c0ce287833ac7a82f697e9b1
yubo <yubo@rivtower.com>
2020-04-20 18:21:30 +0800

    fix

--- src/handler.rs
+++ src/handler.rs
@@ -1,30 +1,75 @@
-use ethereum_types::H160;
+use async_std::prelude::*;
+use async_std::task;
+use cita_tool::{
+    client::basic::Client,
+    protos::blockchain::{Transaction, UnverifiedTransaction},
+};
+use ethereum_types::{H160, H256};
+use futures::{channel::mpsc, select, FutureExt, SinkExt};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+use async_std::net::TcpStream;
 
-type Address = H160;
+pub type Sender<T> = mpsc::UnboundedSender<T>;
+pub type Receiver<T> = mpsc::UnboundedReceiver<T>;
+pub type Address = H160;
 
-const type_HB :u8 = 0;
-const type_PK_REQ :u8 = 1;
-const type_PK_RES :u8 = 2;
-const type_SIGN_REQ :u8 = 3;
-const type_SIGN_RES :u8 = 4;
-const type_URL_REQ :u8 = 7;
-const type_URL_RES :u8 = 8;
-const type_ACCOUNT_REQ :u8 = 9;
-const type_ACCOUNT_RES :u8 = 10;
-const type_TOBE_SENT_DATA :u8 = 11;
+const TYPE_HB: u8 = 0;
+const TYPE_PK_REQ: u8 = 1;
+const TYPE_PK_RES: u8 = 2;
+const TYPE_SIGN_REQ: u8 = 3;
+const TYPE_SIGN_RES: u8 = 4;
+const TYPE_URL_REQ: u8 = 7;
+const TYPE_URL_RES: u8 = 8;
+const TYPE_ACCOUNT_REQ: u8 = 9;
+const TYPE_ACCOUNT_RES: u8 = 10;
+const TYPE_TOBE_SENT_DATA: u8 = 11;
 
-#[derive(clone,debug)]
-pub struct {
-    rpc_url: String,
-    account: Address,
-
+pub enum ChainInfo {
+    UnverifiedTransaction,
+    Sender,
 }
 
+#[derive(Clone)]
+pub struct Iot {
+    pub rpc_url: Option<String>,
+    pub dst_account: Option<Address>,
+    pub my_account: Option<Address>,
+    pub used: bool,
+    pub tcp: Option<TcpStream>,
+    //pub to_chain : Sender<ChainInfo>,
+    //pub from_chain :Receiver<H256>,
+}
 
-pub fn proc_body(ptype: u8,body :&[u8]) {
-    match ptype {
-        type_HB => {
-            
+impl Iot {
+    pub fn new() -> Self {
+        Iot {
+            rpc_url: None,
+            dst_account: None,
+            my_account: None,
+            used: false,
+            tcp:None,
+            //to_chain,
         }
     }
+
+    pub fn set_stream(&mut self,tcp:TcpStream) {
+        self.tcp = Some(tcp);
+    }
+
+    pub fn proc_body(&mut self, ptype: u8, body: &[u8]) -> bool {
+        match ptype {
+            TYPE_HB => {}
+            TYPE_PK_RES => {}
+            _ => {}
+        }
+
+        true
+    }
+
+    // pub async fn send(self,buf: &[u8]) -> async_std::io::Result<()> {
+    //     self.tcp.and_then(|tcp| async { tcp.send(buf).await?}).or()
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,122 @@
-
+mod handler;
 mod payload;
 
-use async_std::io::{self,BufReader};
+use async_std::io::{self, BufReader};
 use async_std::net::{TcpListener, TcpStream};
 use async_std::prelude::*;
+use async_std::sync::{Arc, Mutex};
 use async_std::task;
-use std::sync::{Arc,
-    atomic::{AtomicBool, Ordering}
+use cita_tool::{
+    client::basic::Client,
+    protos::blockchain::{Transaction, UnverifiedTransaction},
 };
+use ethereum_types::{H160, H256};
+use futures::{
+    channel::{mpsc, oneshot},
+    select, FutureExt, SinkExt,
+};
+use handler::ChainInfo;
+use handler::{Iot, Receiver, Sender};
 use payload::Header;
-//#[macro_use]
-//use lazy_static;
 
 lazy_static::lazy_static!{
-    static ref ONE_CONN_FLAG:Arc<AtomicBool> = Arc::new(AtomicBool::new(true));
+    static ref ONE_IOT:Arc<Mutex<Iot>> = Arc::new(Mutex::new(Iot::new()));
 }
 
-fn proc_body(ptype: u8,body :&[u8]) {
-    match ptype {
-        
+async fn network_process(mut net_reader: Option<TcpStream>) -> io::Result<Option<(Header,Vec<u8>)>> {
+    if net_reader.is_none() {
+        return Ok(None);
     }
+    let net_reader = net_reader.unwrap();
+    let mut reader = BufReader::new(net_reader.clone());
+    //loop {
+        println!("network");
+        let mut head_buff = vec![0; 8];
+        reader.read_exact(&mut head_buff).await?;
+        println!("read_exact");
+        let header = Header::parse(&head_buff);
+        let mut body = vec![0; header.len as usize];
+        reader.read_exact(&mut body).await?;
+
+       Ok(Some((header,body)))
+    //}
+
 }
 
+async fn process(
+    op_to_chain: mpsc::UnboundedSender<UnverifiedTransaction>,
+    mut op_from_chain:  mpsc::UnboundedReceiver<H256>,
+    mut op_from_net:  mpsc::UnboundedReceiver<(Header,Vec<u8>)>,
+    iot: Arc<Mutex<Iot>>,
+) -> io::Result<()> {
+    //println!("Accepted from: {}", net_writer.peer_addr()?);
 
-async fn process(stream: TcpStream,is_once : Arc<AtomicBool>) -> io::Result<()> {
-    println!("Accepted from: {}", stream.peer_addr()?);
+    //let res = iot.lock().await.proc_body(header.ptype, &body);
 
-    if !is_once.fetch_and(false, Ordering::SeqCst) {
-        return Err(io::Error::from(io::ErrorKind::AlreadyExists));
+    // if res {
+    //     let tmp = vec![1, 2];
+    //     stream.write(&tmp).await?;
+    // } else {
+    //     //break;
+    // }
+    select! {
+        tx_hash = op_from_chain.next().fuse() => match tx_hash {
+            _ => {}
+        },
+        payload = network_process(iot.lock().await.tcp.clone()).fuse() => {
+
+        }
+        // _ =fut => {
+
+        // }
     }
-    let mut reader = BufReader::new(stream.clone());
-    let mut head_buff = vec![0;8];
-    reader.read_exact(&mut head_buff).await?;
    
-    let header = Header::parse(&head_buff);
-    let mut body = vec![0;header.len as usize];
-    reader.read_exact(&mut body).await?;
-
-
+    if let Some(mut net_writer) = iot.lock().await.tcp.clone() {
+        let tmp = vec![1, 2];
+        net_writer.write(&tmp).await?;
+    }
     
     
-
     Ok(())
 }
 
+async fn chain_loop(
+    chain_to_op: mpsc::UnboundedSender<H256>,
+    chain_from_op: Receiver<UnverifiedTransaction>,
+) {
+}
+
 fn main() -> io::Result<()> {
+    //let (chain_sender,from_chain) = mpsc::unbounded::<H256>();
+    //let iot_static: Arc<Mutex<Iot>> = Arc::new(Mutex::new(Iot::new()));
+    
 
     task::block_on(async {
         let listener = TcpListener::bind("0.0.0.0:18888").await?;
         println!("Listening on {}", listener.local_addr()?);
-        
-        let mut incoming = listener.incoming();
 
+        let (op_to_chain,mut  chain_from_op) = mpsc::unbounded();
+        let (chain_to_op, mut op_from_chain) = mpsc::unbounded();
+        let (net_to_op, mut op_from_net) = mpsc::unbounded();
+
+        let _task = task::spawn(chain_loop(chain_to_op.clone(), chain_from_op));
+
+        let ptsk = task::spawn(async {
+            if let Ok(_) = process(op_to_chain, op_from_chain, op_from_net, ONE_IOT.clone()).await {
+                //;
+            }
+        });
+
+        let mut incoming = listener.incoming();
         while let Some(stream) = incoming.next().await {
             let stream = stream?;
-            
-            task::spawn(async {
-                process(stream,ONE_CONN_FLAG.clone()).await.unwrap();
-            });
+            let iot = ONE_IOT.clone();
+            if iot.lock().await.used == false {
+                iot.lock().await.used = true;
+                iot.lock().await.set_stream(stream.clone());
+            }
         }
+        ptsk.await;
         Ok(())
     })
 }
